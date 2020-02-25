dd075029c7506789c8ca449c3d5b06368456951b
Macatx <60888784+Macatx@users.noreply.github.com>
2020-02-25 19:58:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,36 +5,6 @@
     // Message
     include_once './Functions/message.php';
 
-    // Recebendo dados do formulário
-    $name = $_POST['name'];
-    $email = "mafik.contato@gmail.com";
-    $message = $_POST['mensagem'];
-    $subject = "Mensagem do Site";
-    
-    $headers = "Content-Type: text/html; charset=utf-8\r\n";
-    $headers .= "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    
-    // Dados que serão enviados
-    $corpo = "Formulário da página de contato <br>";
-    $corpo .= "Nome: " . $name . " <br>";
-    $corpo .= "Email: " . $email . " <br>";
-    $corpo .= "Mensagem: " . $message . " <br>";
-    
-    // Email que receberá a mensagem (Não se esqueça de substituir)
-    $email_to = $_POST['email'];
-    
-    // Enviando email
-    $status = mail($email_to, mb_encode_mimeheader($subject, "utf-8"), $corpo, $headers);
-    
-    if ($status):
-      // Enviada com sucesso
-      header('location:index.php?status=sucesso');
-    else:
-      // Se der erro
-      header('location:index.php?status=erro');
-    endif;
-    
 ?>
 
 <!DOCTYPE html>
